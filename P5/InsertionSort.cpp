595b80949b9d9e1c2a9ff0dc73977adde286b612
HritikNegi <91362469+HritikNegi@users.noreply.github.com>
2022-04-11 22:30:58 +0530

    Create InsertionSort.cpp

---
+++ P5/InsertionSort.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n, c=0;
+
+    cout<<"Enter size of array:\n";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter the elements of array:\n";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=1;i<n; i++){
+        int current = arr[i];
+        int j=i-1;
+
+        while(arr[j] > current && j >= 0) {
+            arr [j+1]=arr[j];
+        j--;
+        c++;
+        }
+    arr [j+1]=current;
+    }
+    cout<<"sorted array is:\n";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"Total comparison: "<< c <<endl;
+
+}
